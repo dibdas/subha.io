daf4e079a2ac4cb93c50c087c89dcf0c2fa2174b
dibdas <dibyandu08das@gmail.com>
2019-04-20 23:52:50 +0530

    changes made

--- app.js
+++ app.js
@@ -3,34 +3,33 @@
 const express =require("express");
 const app = express ();
 const bodyParser=require("body-parser");
+var a =['cow','dog','cat']
 
+app.get("/:animal",(request,response) =>{
+    console.log(request.params.animal)
+    var b = (request.params.animal)
+    if(b ===a[0]){
+    response.send("moo");
+    }
+    else if(b==a[1]){
+        response.send("barks")
+    }
+    else if (b==a[2]){
+        response.send("meow")
+    }
+    else{
+        response.send("animal not found")
+    }
 
-app.get("/cow",(request,response) =>{
-    
-    console.log(response)
-    response.send(MOO);
-})
-
-app.get("/dog",(request,response) =>{
-    
-    console.log(response)
-    response.send("BARKS");
-})
-
-app.get("/cat",(request,response) =>{
-    
-    console.log(response)
-    response.send("MEOW");
 })
 
 
 /*
-app.get("/profile/:abcid",(req,res)=>{
-res.send("your id"+req.params.abcid)
+app.get("/profile/id",(req,res)=>{
+res.send("your id"+req.params.id)
 })
+
 */
-
-
 app.listen(3000,()=>{
     console.log("server started")
 })
